cf3e9458a45efaaae62c1ebf4bdb7fa19f04d077
olemen111 <1065339646@qq.com>
2019-10-07 20:57:53 +0800

    10.7

---
+++ olemon/youku/js/backtotop.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+    var obtn = document.getElementById('btn');
+    var clientHeight = document.documentElement.clientHeight;
+    var timer = null;
+    var isTop = true;
+
+    window.onscroll = function(){
+        var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+        if(osTop >= clientHeight){
+            obtn.style.display = 'block';
+        }else{
+            obtn.style.display = 'none';
+        }
+        if(!isTop){
+
+            clearInterval(timer);
+        }
+        isTop = false;
+
+    }
+    obtn.onclick = function(){
+        timer = setInterval(function(){
+            var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+            var speed = Math.floor(-osTop / 6);
+            document.documentElement.scrollTop = document.body.scrollTop = osTop + speed;
+            isTop =true;
+            if(osTop == 0){
+                clearInterval(timer);
+            }
+        },30);
+    }
+}

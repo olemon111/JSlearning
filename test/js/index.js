6d928d591e7dbf102d5de279370a25f32050a258
olemon111 <55992283+olemon111@users.noreply.github.com>
2019-11-02 18:27:57 +0800

    Add files via upload

--- test/js/index.js
+++ test/js/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-10-29 18:07:31
- * @LastEditTime: 2019-11-02 10:12:19
+ * @LastEditTime: 2019-11-02 17:58:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \test\js\index.js
@@ -9,173 +9,183 @@
 
 
 var oUl = document.getElementsByTagName("ul")[0];
-var obtn=document.getElementsByTagName ("button");
-var aUl= document.getElementById("UL");
-var oCheckbox=document.getElementsByClassName("Checkbox");
+var obtn = document.getElementsByTagName ("button");
+var aUl = document.getElementById("UL");
+var oCheckbox = document.getElementsByClassName("Checkbox");
 var oLi = oUl.getElementsByTagName("li");
-var oactive= document.getElementById("active");
-var ocompleted= document.getElementById("completed");
-var oall= document.getElementById("all");
-var oclear= document.getElementById("clear");
-var oselected=document.getElementsByClassName("selected");
-var oselectAll=document.getElementById("selectAll");
-var ocontent= document.getElementById("content");
-var ounselected=document.getElementsByClassName("");
+var oactive = document.getElementById("active");
+var ocompleted = document.getElementById("completed");
+var oall = document.getElementById("all");
+var oclear = document.getElementById("clear");
+var oselected = document.getElementsByClassName("selected");
+var oselectAll = document.getElementById("selectAll");
+var ocontent = document.getElementById("content");
+var ounselected = document.getElementsByClassName("");
 var oInput = document.getElementsByTagName("input")[0];
-/*var oFoot = document.getElementById("foot");*/
-var NewInput=document.getElementsByClassName("newinput");
+var oFoot = document.getElementById("foot");
+var NewInput = document.getElementsByClassName("newinput");
 
 
-
-
-window.onload = function(){
-
-    
+window.onload = function() {
+    oFoot.style.display = "none";
     oInput.onkeydown = function(event) {
-        var event = event || window.event;
-        
+        var event = event || window.event;       
         if(event.keyCode == 13 && oInput.value!="") {
             var data = oInput.value;
             addData(data);
             oInput.value = "";
-            itemNumber();
+            itemNumber();           
         }
-  /*      for(var i=0; i<deletebutton.length;i++){
-          deletebutton.className="delete";
-       }
-*/
-
-        for(var j=0;j<oCheckbox.length;j++){
-    
+        if(oLi.length == 0) {
+            oFoot.style.display = "none";
+          }
+          else {
+            oFoot.style.display = "block";
+          }
+        for(var j = 0;j < oCheckbox.length;j++){
             oCheckbox[j].addEventListener("click",function () { 
-                if(this.parentNode.className=="selected"){
-                    this.parentNode.className="";
-                    
-                    oCheckbox[j].style.backgroundImage = "url('css\images\未选.png')";
+                if(this.parentNode.className == "selected"){
+                    this.parentNode.className = "";               
                 }
                 else{
-                    this.parentNode.className="selected"; 
+                    this.parentNode.className = "selected"; 
                 }
                 itemNumber();
             })
            
         }
-
-
-        for(var i=0;i<oLi.length;i++) {
-            if(oLi[i].style.display!="none")
-            oLi[i].style.display="block";
+        for(var i = 0;i < oLi.length;i++) {
+            if(oLi[i].style.display != "none")
+            	oLi[i].style.display = "block";
+           	 	oLi[i].ondblclick = function() {
+              	var previousvalue = this.getElementsByTagName("span")[0].innerHTML;
+              	var newInput = document.createElement("input");
+                if(NewInput.length <=0) {
+                	this.appendChild(newInput);
+             		  newInput.className = "newinput";
+              		newInput.value = previousvalue;
+              		this.getElementsByTagName("span")[0].innerHTML = "";
+                }              
+              NewInput[0].addEventListener("keydown",function(event) {
+               var event = event || window.event;
+               if(event.keyCode == 13) {
+									if(NewInput[0].value != "") {
+									NewInput[0].parentNode.getElementsByTagName("span")[0].innerHTML = NewInput[0].value;
+									NewInput[0].parentNode.removeChild(NewInput[0]);
+									}
+									else {
+									NewInput[0].parentNode.parentNode.removeChild(NewInput[0].parentNode);
+									}
+									itemNumber();
+                }
+              })  
+                NewInput[0].addEventListener("blur",function(){
+									if(NewInput[0].value != ""){
+										NewInput[0].parentNode.getElementsByTagName("span")[0].innerHTML = NewInput[0].value;
+										NewInput[0].parentNode.removeChild(NewInput[0]);
+										}
+									else {
+										NewInput[0].parentNode.parentNode.removeChild(NewInput[0].parentNode);
+									}
+									itemNumber();
+                })  
+           }
         }
+  
 
         oactive.addEventListener("click",function(){
-            for(var k=0;k<oLi.length;k++){
-                if(oLi[k].className=="selected")
-                    oLi[k].style.display="none";
+            for(var k = 0;k <oLi.length;k++){
+                if(oLi[k].className == "selected")
+                    oLi[k].style.display = "none";
                 else 
-                    oLi[k].style.display="block";
+                    oLi[k].style.display = "block";
             }
-            
-    })
-
+				})
         ocompleted.addEventListener("click",function(){
-            for(var k=0;k<oLi.length;k++){
-                if(oLi[k].className=="selected")
-                    oLi[k].style.display="block";
+            for(var k = 0;k < oLi.length;k++){
+                if(oLi[k].className == "selected")
+                    oLi[k].style.display = "block";
                 else
-                    oLi[k].style.display="none";
+                    oLi[k].style.display = "none";
             }
         })
-
         oall.addEventListener("click",function(){
-            for(var k=0;k<oLi.length;k++){
-                    oLi[k].style.display="block";
+            for(var k = 0;k <oLi.length;k++){
+                    oLi[k].style.display = "block";
             }
         })
-
         oclear.addEventListener("click",function(){
-            for(var k=0;k<oselected.length;k++){
+            for(var k = 0;k < oselected.length;k++){
                 oselected[k].parentNode.removeChild(oselected[k]);
             }
             itemNumber();
         })
 
-        var flag=false;
+        var flag = false;
         oselectAll.addEventListener("click",function () {
-
-            if(flag){
-                for(var k=0; k<oLi.length; k++){
+            if(flag) {for(var k = 0; k < oLi.length; k++){
                     oLi[k].className="";
                 }
-                for(var k=0;k<oCheckbox.length;k++){
-                    oCheckbox[k].checked=false;
+                for(var k = 0;k < oCheckbox.length;k++){
+                    oCheckbox[k].checked = false;
                 }
-                flag=false;
+                flag = false;
             }
-            else{
-
-                for(var k=0;k<oLi.length;k++){
-                    oLi[k].className="selected";
+            else {for(var k = 0;k <oLi.length;k++){
+                    oLi[k].className = "selected";
                 }
-                for(var k=0;k<oCheckbox.length;k++){
-                    oCheckbox[k].checked=true;
+                for(var k = 0;k < oCheckbox.length;k++){
+                    oCheckbox[k].checked = true;
                 }
-                flag=true;
+                flag = true;
             }
             itemNumber();
 
-          })
+       })
+    }
+}
 
 
-}
-}
 
 function addData(data){
     var createLi = document.createElement("li");
     var createBtn = document.createElement("button");
     var createSpan = document.createElement("span");
     var createCheckbox = document.createElement("input");
-
     createCheckbox.type = "checkbox";
     createCheckbox.className = "Checkbox";
     createSpan.innerHTML = data;
     createLi.appendChild(createCheckbox);
     createLi.appendChild(createSpan);
     createLi.appendChild(createBtn);
+    createBtn.className = "delete";
     createLi.className = "";
-
     oUl.appendChild(createLi);
-
-    for(var i=0; i<obtn.length; i++){
-        obtn[i].onclick=function() 
-        {
+    for(var i = 0; i < obtn.length; i++){
+        obtn[i].onclick = function() {
           aUl.removeChild(this.parentNode);
           itemNumber();
         }
-        
-   }
-
-   
+  	}
 }
 
-for(var j=0;j<oCheckbox.length;j++){
-    
+for(var j = 0;j < oCheckbox.length;j++) { 
     oCheckbox[j].addEventListener("click",function () { 
-        if(this.parentNode.className=="selected"){
-            this.parentNode.className="";
+        if(this.parentNode.className == "selected"){
+            this.parentNode.className = "";
         }
         else{
-            this.parentNode.className="selected"; 
+            this.parentNode.className = "selected"; 
         }
     })
     itemNumber();
 }
-
 function itemNumber() { 
     var a = oLi.length-oselected.length;
     if(a>1){
-        ocontent.innerHTML=(a+' items left');
+        ocontent.innerHTML = (a + ' items left');
     }
     else {
-        ocontent.innerHTML=(a+' item left');
+        ocontent.innerHTML = (a + ' item left');
     }
- }
+}
